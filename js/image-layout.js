7883b1490b3bc189335da5b5eabce77952aacbb7
Wally Noveno <wally@quadrantalpha.com>
2018-09-05 13:43:14 +0800

    first commit

---
+++ js/image-layout.js
@@ -0,0 +1,25 @@
+// Demo by Dan Rose.
+// See: http://www.sitepoint.com/recreating-google-images-search-layout-css
+
+
+
+//bind click events
+var $cell = $('.image__cell');
+
+$cell.find('.image--basic').click(function() {
+  var $thisCell = $(this).closest('.image__cell');
+  
+  if ($thisCell.hasClass('is-collapsed')) {
+    $cell.not($thisCell).removeClass('is-expanded').addClass('is-collapsed');
+    $thisCell.removeClass('is-collapsed').addClass('is-expanded');
+  } else {
+    $thisCell.removeClass('is-expanded').addClass('is-collapsed');
+  }
+});
+
+$cell.find('.expand__close').click(function(){
+  
+  var $thisCell = $(this).closest('.image__cell');
+  
+  $thisCell.removeClass('is-expanded').addClass('is-collapsed');
+});
